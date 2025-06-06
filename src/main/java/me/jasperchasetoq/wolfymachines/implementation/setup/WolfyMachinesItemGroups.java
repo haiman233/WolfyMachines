f5211f22fa0a3880c27fecb59b92a8eef705c2ba
haiman <83174104+haiman233@users.noreply.github.com>
2025-06-06 19:18:52 +0800

    汉化

--- src/main/java/me/jasperchasetoq/wolfymachines/implementation/setup/WolfyMachinesItemGroups.java
+++ src/main/java/me/jasperchasetoq/wolfymachines/implementation/setup/WolfyMachinesItemGroups.java
@@ -1,15 +1,16 @@
 package me.jasperchasetoq.wolfymachines.implementation.setup;
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+
 import io.github.thebusybiscuit.slimefun4.api.items.groups.NestedItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.groups.SubItemGroup;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import me.jasperchasetoq.wolfymachines.WolfyMachines;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 
 public class WolfyMachinesItemGroups {
 
-    public static final NestedItemGroup NestedGeneral = new NestedItemGroup(new NamespacedKey(WolfyMachines.getInstance(), "GENERAL"), new CustomItemStack(Material.FURNACE, "&fWolfy Machines"));
-    public static final SubItemGroup Machines = new SubItemGroup(new NamespacedKey(WolfyMachines.getInstance(), "MACHINES"), NestedGeneral, new CustomItemStack(Material.BLAST_FURNACE, "&fMachines"));
+    public static final NestedItemGroup NestedGeneral = new NestedItemGroup(new NamespacedKey(WolfyMachines.getInstance(), "GENERAL"), new CustomItemStack(Material.FURNACE, "&f狼之机器"));
+    public static final SubItemGroup Machines = new SubItemGroup(new NamespacedKey(WolfyMachines.getInstance(), "MACHINES"), NestedGeneral, new CustomItemStack(Material.BLAST_FURNACE, "&f机器"));
 
 }
